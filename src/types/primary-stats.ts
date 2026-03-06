ca18ab4d2270a45b865e8d857747caf260ba76af
hiptodude3 <hiptodude4@gmail.com>
2026-03-06 23:09:17 +0200

    Add PrimaryStats interface and factory function

---
+++ src/types/primary-stats.ts
@@ -0,0 +1,12 @@
+export interface PrimaryStats {
+  str: number;
+  dex: number;
+  agi: number;
+  end: number;
+  int: number;
+  wis: number;
+}
+
+export function createPrimaryStats(): PrimaryStats {
+  return { str: 10, dex: 10, agi: 10, end: 10, int: 10, wis: 10 };
+}
