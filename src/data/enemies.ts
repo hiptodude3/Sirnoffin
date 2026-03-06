34c2e4c504341a648b3f4d4ae5e9ee7b5c789d7e
hiptodude3 <hiptodude4@gmail.com>
2026-03-07 00:32:31 +0200

    Update enemies.ts

--- src/data/enemies.ts
+++ src/data/enemies.ts
@@ -1,10 +1,10 @@
-import { EnemyTemplate } from '../entities/Enemy.js';
+import { EnemyTemplate } from '../types/enemy.js';
 
 export const FOREST_ENEMIES: EnemyTemplate[] = [
   { name: 'Slime',         hp: 40, attack: 15, defense: 8,  speed: 4,  xp: 15, gold: [3, 8]  },
   { name: 'Goblin',        hp: 55, attack: 22, defense: 12, speed: 8,  xp: 25, gold: [5, 15] },
   { name: 'Wolf',          hp: 45, attack: 25, defense: 8,  speed: 12, xp: 20, gold: [4, 10] },
-  { name: 'Forest Spider', hp: 35, attack: 28, defense: 5,  speed: 11, xp: 22, gold: [4, 12] }
+  { name: 'Forest Spider', hp: 35, attack: 28, defense: 5,  speed: 11, xp: 22, gold: [4, 12] },
 ];
 
 export function randomEnemy(pool: EnemyTemplate[]): EnemyTemplate {
