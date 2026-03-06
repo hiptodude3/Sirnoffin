6f76eb21c9c59f611303729cbea337b2376d712f
hiptodude3 <hiptodude4@gmail.com>
2026-03-06 23:09:53 +0200

    Add DerivedStats interface for character stats

---
+++ src/types/derived-stats.ts
@@ -0,0 +1,14 @@
+export interface DerivedStats {
+  maxHp: number;
+  maxMp: number;
+  maxSp: number;
+  currentHp: number;
+  currentMp: number;
+  currentSp: number;
+  physMitigation: number;
+  magMitigation: number;
+  evade: number;
+  parry: number;
+  resist: number;
+  block: number;
+}
