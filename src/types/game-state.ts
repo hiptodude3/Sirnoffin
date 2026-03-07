9f5c6e4ac35015c338e4b2cbe961159abb2ee1f7
hiptodude3 <hiptodude4@gmail.com>
2026-03-07 04:14:39 +0200

    Update game-state.ts

--- src/types/game-state.ts
+++ src/types/game-state.ts
@@ -2,7 +2,7 @@ import { Character } from './character.js';
 
 export type Screen =
   | 'title' | 'create' | 'town'
-  | 'battle' | 'stats' | 'inventory';
+  | 'battle' | 'stats' | 'inventory' | 'settings';
 
 export interface GameState {
   screen: Screen;
